10ecf0c0507a30f05567e8d12dbb9637a17763d2
gabrielLaino <lainogabriel14@gmail.com>
2025-03-06 21:44:04 -0300

    utilizando cookies

---
+++ src/@type/knex.d.ts
@@ -0,0 +1,13 @@
+import { Knex } from "knex";
+
+declare module 'knex/types/tables' {
+  export interface Tables {
+    transactions: {
+      id: string,
+      title: string,
+      amount: number,
+      type?: string,
+      session_id?: string
+    }
+  }
+}
--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -5,6 +5,32 @@ import { title } from "process";
 import crypto from 'node:crypto';
 
 export const transactionsRoutes = async (app: FastifyInstance) => {
+  app.get('/', async (req, res) => {
+    const transactions = await knex('transactions').select('*');
+
+    return res.status(200).send({transactions})
+  });
+
+  app.get('/:id', async (req, res) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid()
+    });
+
+    const {id} = getTransactionParamsSchema.parse(req.params);
+
+    const transaction = await knex('transactions').where('id', id).first();
+
+    return res.status(200).send({transaction});
+  });
+
+  app.get('/summary', async (req, res) => {
+    const summary = await knex('transactions').sum('amount', {
+      as: 'amount'
+    }).first();
+
+    return res.status(200).send({summary})
+  });
+
   app.post('/', async (req, res) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
@@ -14,11 +40,21 @@ export const transactionsRoutes = async (app: FastifyInstance) => {
 
     const {title, amount, type} = createTransactionBodySchema.parse(req.body);
 
+    let sessionId = req.cookies.sessionId
+
+    if(!sessionId) {
+      sessionId = crypto.randomUUID()
+      res.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 30
+      })
+    }
+
     await knex('transactions').insert({
       id: crypto.randomUUID(),
       title,
       amount: type === 'credit' ? amount : amount * -1,
-      type
+      session_id: sessionId
     });
 
     res.status(201).send('feito');
